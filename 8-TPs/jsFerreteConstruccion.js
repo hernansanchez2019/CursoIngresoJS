c5cf8a9d5bebee319c9f725ccd8511f2c8626ab3
hernansanchez2019 <52495092+hernansanchez2019@users.noreply.github.com>
2019-07-31 11:44:44 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -4,31 +4,54 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+function Rectangulo() {
 
 
-var Largo;
-var Ancho;
-var Radio;
+    var Largo;
+    var Ancho;
+    var Radio;
+    var resultado;
 
-Largo = parseInt(document.getElementById("Largo").value);
-Ancho = parseInt(document.getElementById("Ancho").value);
-Radio = parseInt(document.getElementById("Radio").value);
+    Largo = parseInt(document.getElementById("Largo").value);
+    Ancho = parseInt(document.getElementById("Ancho").value);
+    resultado = Largo * Ancho * 3;
 
-Radio = 
-
-
+    alert(" La cantidad de alambre a comprar es " + resultado + "");
 
 }
 
 
+function Circulo() {
+    var Largo;
+    var Ancho;
+    var Radio;
+    var resultado;
 
-function Circulo () 
-{
-	
+    Largo = parseInt(document.getElementById("Largo").value);
+    Ancho = parseInt(document.getElementById("Ancho").value);
+    Radio = parseFloat(document.getElementById("Radio").value);
+    resultado = Radio * 3.14 * 3;
+
+    alert(" La cantidad de alambre a comprar es " + resultado);
+
+
 }
-function Materiales () 
-{
-	
+function Materiales() {
+
+    var Largo;
+    var Ancho;
+    var Radio;
+    var resultado;
+    var cemento;
+    var cal;
+
+    Largo = parseInt(document.getElementById("Largo").value);
+    Ancho = parseInt(document.getElementById("Ancho").value);
+    cemento = Largo * Ancho * 2;
+    cal = Largo * Ancho * 3;
+
+    alert(" Se necesitan " + cemento + " bolsas de cemento y " + cal + " bolsas de cal ");
+
+
 }
+
